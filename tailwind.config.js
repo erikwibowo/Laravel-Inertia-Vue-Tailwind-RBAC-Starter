28764c3e08b28470cc48a6a5b6c7840f53c3d600
erikwibowo <erikwibo@gmail.com>
2022-06-06 12:58:11 +0700

    install nunito font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
         "./resources/**/*.vue",
     ],
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                'Nunito': ["Nunito"],
+            },
+        },
     },
     plugins: [require("@tailwindcss/forms")],
 };
